cb510b432655e76dcd95b3fa8f169ceeb6b43c35
Loften <myloften@users.noreply.github.com>
2025-03-22 10:58:41 +0800

    fix: update layout and next config for static export

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  assetPrefix: '/',
+  basePath: '',
   images: {
     unoptimized: true,
     remotePatterns: [
